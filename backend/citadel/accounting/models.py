c950cbb2b324dbb0255671b0736a9bd03be9cfff
kellymweu <mweukelly@gmail.com>
2024-06-24 17:06:05 +0300

    initializes the apps and makes db migrations

--- backend/citadel/accounting/models.py
+++ backend/citadel/accounting/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+#Stocks Counts
+class Item(models.Model):
+    name = 
+    units = 
+    tax_type =
+    SKU =
+    marked_price = 
+
+class Purchase(models.Model):
+    pass
+
+class Sale(models.Model):
+    item = 
+    quantity =
+    unit_price = 
+    sub_total =  
+
